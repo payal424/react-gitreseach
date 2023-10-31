5da5a1194800abb4f56db218c94a2e11166f0772
payal424 <144161609+payal424@users.noreply.github.com>
2023-11-01 00:54:28 +0530

    changes for gitresearch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Callgit from './bct/callgit';
+
+function App() {
+  return (
+    <div className="App">
+      <Callgit></Callgit>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/bct/callgit.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const Callgit = () => {
+    const [state,setState] = useState('')
+    const [data,setData] = useState([])
+    const [isValid, setIsValid]  = useState(false)
+    useEffect(() => {
+        if(state.length !== 0){setIsValid(true)}
+        else {setIsValid(false)}
+    }, [state])
+    const clickHandler = (e) => {
+        fetch(`https://api.github.com/users/${state}/repos`).then(res => res.json()).then(data => setData(data))
+
+    };
+    const changeHandler = (e) => {
+       // e.preventDefault()
+        setState(e.target.value)
+        
+    }
+    console.log(state)
+    return(<div>
+        hellow!
+        <div>
+            <label>enter user name: </label>
+            <input onChange={changeHandler}></input>
+            <div>
+                <button disabled = {!isValid} onClick={clickHandler}>submit</button>
+            </div>
+        </div>
+        <div>
+            {data.map(i => { return(
+                <div key = {i.id} style={{borderBottom : '1px solid'}}>
+                    {i.name} {i.full_name}
+                </div>
+            )})}
+        </div>
+    </div>);
+}
+export default Callgit;
